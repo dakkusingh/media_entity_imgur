5520950d2b23ae1f2a31e48a3bf4fcd8e74880c1
dakku <dakku@97634.no-reply.drupal.org>
2017-04-14 02:02:57 +0100

    adding formatter file

---
+++ src/Plugin/Field/FieldFormatter/ImgurEmbedFormatter.php
@@ -0,0 +1,39 @@
+imgur<?php
+
+namespace Drupal\media_entity_imgur\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\media_entity\EmbedCodeValueTrait;
+
+/**
+ * Plugin implementation of the 'imgur_embed' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "imgur_embed",
+ *   label = @Translation("Imgur embed"),
+ *   field_types = {
+ *     "link", "string", "string_long"
+ *   }
+ * )
+ */
+class ImgurEmbedFormatter extends FormatterBase {
+
+  use EmbedCodeValueTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = array();
+    foreach ($items as $delta => $item) {
+      $element[$delta] = [
+        '#type' => 'markup',
+        '#markup' => $this->getEmbedCode($item),
+        '#allowed_tags' => ['img', 'a', 'script'],
+      ];
+    }
+    return $element;
+  }
+
+}
